71ef022efef1e7a8e77923a2e483987fcdddcea9
DanielGeek <daniel30081990@gmail.com>
2017-04-10 12:34:36 -0400

    Created app object with Vue.js
    Object created and added as invoice id
    Added data in the Vue.js policies in the header section and the sales
    invoice

--- js/datos.js
+++ js/datos.js
@@ -1,14 +1,9 @@
-var app1 = new Vue({
-    el: '#app-1',
+
+var app = new Vue({
+    el: '#app',
     data: {
         name: 'Miguel Uribe MV S.A.S',
-        //    apellido: 'juan'
-    }
-});
-
-var app2 = new Vue({
-    el: '#app-2',
-    data: {
+        //    apellido: 'juan',
         parrafos: [
             {
                 parrafo: 'ACTIVIDAD ECONÓMICA 7500'
@@ -19,6 +14,51 @@ var app2 = new Vue({
             {
                 parrafo: 'IVA Regimen Común'
             }
-    ]
+    ],
+         parrafos2: [
+           
+            {
+                parrafo2: 'Nit. 900643855-4'
+            },
+            {
+                parrafo2: '3212900914'
+            },
+            {
+                parrafo2: 'Kr 79 No. 127C - 45 IN 2 AP 101'
+            },
+            {
+                parrafo2: 'Bogotá'
+            },
+            {
+                parrafo2: 'Colombia'
+            }
+    ],
+        name2: 'IKE ASISTENCIA COLOMBIA',
+        parrafos3: [
+            {
+                parrafo3: 'NIT. 900106251-3'
+            },
+            {
+                parrafo3: 'Cll 93B 17 - 25 Piso 6'
+            },
+            {
+                parrafo3: 'Edif. Centro Internacional de Negocios'
+            },
+            {
+                parrafo3: 'Bogotá'
+            },
+            {
+                parrafo3: 'Colombia'
+            }
+        ],
+        factura: 'Factura de Venta',
+        numero: 'Número: ',
+        numDato: '1607',
+        fecha: 'Fecha: ',
+        fechaDato: '17/02/2017',
+        vencimiento: 'Vencimiento: ',
+        venDato: '19/03/2017',
+        referencia: 'Referencia: ',
+        refDato: 'Banco Popular'
     }
 });
