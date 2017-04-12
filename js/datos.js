48756a7912ca01632e9273cdb2c034de6ac6ff15
daniel angel <daniel30081990@gmail.com>
2017-04-12 02:05:28 -0400

    Add footer and reestruct code datos.js

--- js/datos.js
+++ js/datos.js
@@ -1,40 +1,44 @@
 var app = new Vue({
     el: '#app',
     data: {
-        name: 'Miguel Uribe MV S.A.S',
-        //    apellido: 'juan',
-        parrafos: {
-            actividad: 'ACTIVIDAD ECONÓMICA 7500',
-            tarifa: 'TARIFA. ICA 9.66X1000',
-            iva: 'IVA Regimen Común'
+        company: {
+            name: 'Miguel Uribe MV S.A.S',
+            activity: {
+                name: 'ACTIVIDAD ECONÓMICA 7500',
+                ica: 'TARIFA. ICA 9.66X1000',
+                regime: 'IVA Regimen Común'
+            },
+            info: {
+                nit: 'Nit. 900643855-4',
+                phone: '3212900914',
+                address: 'Kr 79 No. 127C - 45 IN 2 AP 101',
+                city: 'Bogotá',
+                country: 'Colombia'
+            }
         },
-        parrafos2: {
-            nit: 'Nit. 900643855-4',
-            numero: '3212900914',
-            dir: 'Kr 79 No. 127C - 45 IN 2 AP 101',
-            city: 'Bogotá',
-            country: 'Colombia'
+        customerInfo: {
+            name: 'IKE ASISTENCIA COLOMBIA',
+            customer: {
+                nit: 'NIT. 900106251-3',
+                dir: 'Cll 93B 17 - 25 Piso 6',
+                local: 'Edif. Centro Internacional de Negocios',
+                city: 'Bogotá',
+                country: 'Colombia'
+            }
+        },
+        invoiceInfo: {
+            name: 'Factura de Venta',
+            invoice: {
+                numero: 'Número: ',
+                numDato: '1607',
+                fecha: 'Fecha: ',
+                fechaDato: '17/02/2017',
+                vencimiento: 'Vencimiento: ',
+                venDato: '19/03/2017',
+                referencia: 'Referencia: ',
+                refDato: 'Banco Popular',
+            }
 
-        },
-        name2: 'IKE ASISTENCIA COLOMBIA',
-
-        parrafos3: {
-            nit: 'NIT. 900106251-3',
-            dir: 'Cll 93B 17 - 25 Piso 6',
-            local: 'Edif. Centro Internacional de Negocios',
-            city: 'Bogotá',
-            country: 'Colombia'
-        },
-        parrafos4: {
-            factura: 'Factura de Venta',
-            numero: 'Número: ',
-            numDato: '1607',
-            fecha: 'Fecha: ',
-            fechaDato: '17/02/2017',
-            vencimiento: 'Vencimiento: ',
-            venDato: '19/03/2017',
-            referencia: 'Referencia: ',
-            refDato: 'Banco Popular',
         },
         thead: {
             description: 'Descripción: ',
@@ -99,7 +103,41 @@ var app = new Vue({
                 price: '100.000',
                 total: '100.000,0'
             }
-        }
+        },
+        invoiceNotes: {
+            title: 'Nota',
+            note: 'Nos acogemos a los beneficios establecidos en la Ley 1429 de 2010. Favor no aplicar Retención en la Fuente. CONSIGNAR EN BANCOLOMBIA CUENTA DE AHORROS No. 20611685999 A NOMBRE DE MIGUEL URIBE MV S.A.S'
+        },
+        invoiceTotal: {
+            head: {
+                headSubtotal: 'Subtotal',
+                headIva: '+IVA 16%',
+                headTotal: 'Total',
+                headRete: '-Rete IVA 15%',
+                headRetefuente: '-Retefuente 3.5%',
+                headSaldo: 'Saldo'
+            },
+            headData: {
+                dataSubtotal: '930.000',
+                dataIva: '148,800',
+                dataTotal: '1,078,800',
+                dataRete: '22,320',
+                dataRetefuente: '32,550',
+                dataSaldo: '1,023,930'
+            }
+        },
+        totalFirmas: {
+            firma1: {
+                lineaA: '____________________________________________',
+                pFirma: 'Elaborado por'
+            },
+            firma2: {
+                lineaA: '____________________________________________',
+                pFirma: 'Aceptado, Firma y/o Sello'
+            }
+        },
+        footer: 'RESOLUCION DIAN 320001302741 DEL 19 DE AGOSTO DE 2015. NUMERACIÓN AUTORIZADA POR COMPUTADOR 507 - 2000'
+
 
     }
 });
